4c855d5dca26c9317650d85bad16ed6cbc353fa0
jschrbr <schrbrr@gmail.com>
2020-04-29 10:27:42 +1000

    Updated connection for production deployment

--- config/connection.js
+++ config/connection.js
@@ -26,13 +26,7 @@
 const mysql = require("mysql");
 const util = require("util");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgerDB",
-});
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function (err) {
   if (err) throw err;
