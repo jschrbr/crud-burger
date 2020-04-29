bb78eaf8bccc1065a0d174a3cb2d6c013509284f
jschrbr <schrbrr@gmail.com>
2020-04-29 11:55:48 +1000

    Cleaned up regex

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const util = require("util");
-const myRegex = /mysql:\/\/([\w\d]+):([\w\d]+)@([\w\d.-]+):[\w\d]+\/([\w\d]+)/i;
+const myRegex = /[\w]+:..([\w]+):([\w]+)@([\w.-]+):[\w]+\/([\w]+)/i;
 
 const [_, usr, pass, hst, db] = myRegex.exec(process.env.JAWSDB_URL);
 
